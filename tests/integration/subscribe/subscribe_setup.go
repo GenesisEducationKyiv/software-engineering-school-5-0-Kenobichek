ed50446f519522e6c345c2fd5e2452679e0eb90c
Olesia Lesechko <lesechklesia@gmail.com>
2025-06-24 12:28:28 +0300

    test: improve error handling and messages in subscription handler

--- tests/integration/subscribe/subscribe_setup.go
+++ tests/integration/subscribe/subscribe_setup.go
@@ -6,17 +6,10 @@ import (
 	"Weather-Forecast-API/internal/repository/subscriptions"
 	"Weather-Forecast-API/internal/routes"
 	"Weather-Forecast-API/internal/services/subscription"
-	"Weather-Forecast-API/tests/testdb"
 	"bytes"
-	"context"
 	"database/sql"
-	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"log"
 	"mime/multipart"
-
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"sync"
@@ -100,98 +93,6 @@ func newAppRouterWithDB(t *testing.T, db *sql.DB, notifSvc notificationManager) 
 	routes.NewService(weatherHandler, subscribeHandler, router).RegisterRoutes()
 
 	return router
-}
-
-func TestSubscribeAPI(t *testing.T) {
-	pg := testdb.New(t)
-	database := pg.SQL
-
-	testCases := []struct {
-		name               string
-		requestBody        map[string]string
-		expectedStatusCode int
-		expectConfirmation bool
-		expectedBody       string
-	}{
-		{
-			name: "Successful subscription",
-			requestBody: map[string]string{
-				"email":     "test@example.com",
-				"city":      "Kyiv",
-				"frequency": "daily",
-			},
-			expectedStatusCode: http.StatusOK,
-			expectedBody:       `{"message":"` + subscribe.MsgSubscriptionSuccess + `"}`,
-			expectConfirmation: true,
-		},
-		{
-			name: "Missing email",
-			requestBody: map[string]string{
-				"city":      "Kyiv",
-				"frequency": "daily",
-			},
-			expectedStatusCode: http.StatusBadRequest,
-			expectedBody:       `{"message":"` + subscribe.ErrInvalidInput.Error() + `"}`,
-			expectConfirmation: false,
-		},
-		{
-			name: "Already subscribed",
-			requestBody: map[string]string{
-				"email":     "test@example.com",
-				"city":      "Kyiv",
-				"frequency": "daily",
-			},
-			expectedBody:       `{"message":"` + subscribe.ErrAlreadySubscribed.Error() + `"}`,
-			expectedStatusCode: http.StatusConflict,
-			expectConfirmation: false,
-		},
-	}
-
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			dbMutex.Lock()
-			defer dbMutex.Unlock()
-
-			mockNotif := newMockNotificationService()
-			appSrv := newTestServer(t, newAppRouterWithDB(t, database, mockNotif))
-
-			body, contentType := multipartBody(t, tc.requestBody)
-
-			reqURL := appSrv.URL + "/api/subscribe"
-			ctx := context.Background()
-			req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, body)
-			require.NoError(t, err)
-
-			req.Header.Set("Content-Type", contentType)
-
-			resp, err := http.DefaultClient.Do(req)
-			require.NoError(t, err)
-			defer func() {
-				if err := resp.Body.Close(); err != nil {
-					log.Println("failed to close response body")
-					return
-				}
-			}()
-
-			assert.Equal(t, tc.expectedStatusCode, resp.StatusCode)
-			bodyBytes, _ := io.ReadAll(resp.Body)
-
-			log.Printf("response body: %s", string(bodyBytes))
-			if tc.expectedBody != "" {
-				require.True(t, json.Valid([]byte(tc.expectedBody)), "expectedBody is not valid JSON")
-				require.True(t, json.Valid(bodyBytes), "response body is not valid JSON")
-				assert.JSONEq(t, tc.expectedBody, string(bodyBytes), "response body mismatch")
-			}
-			if tc.expectConfirmation {
-				require.Len(t, mockNotif.sentConfirmations, 1,
-					"expected confirmation to be sent")
-			} else {
-				require.Len(t, mockNotif.sentConfirmations, 0,
-					"did not expect confirmation to be sent")
-			}
-		})
-	}
 }
 
 func newTestServer(t *testing.T, h http.Handler) *httptest.Server {
--- tests/integration/weather/weather_test.go
+++ tests/integration/weather/weather_test.go
@@ -1,4 +1,4 @@
-package weather_test_test
+package weather_test
 
 import (
 	"Weather-Forecast-API/external/openweather"
